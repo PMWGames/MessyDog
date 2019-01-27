214eace5b8b66dd688dcbac96eade66b875a5d61
Rafael Araújo <rafaelskate7@gmail.com>
2019-01-27 01:08:48 -0300

    novas adicoes da madrugada

--- app/src/main/java/com/elementalspin/pmwgames/messydog/CenaGame.java
+++ app/src/main/java/com/elementalspin/pmwgames/messydog/CenaGame.java
@@ -18,9 +18,17 @@ public class CenaGame extends AGScene {
     private static int PARADO = 0, ANDANDO = 1;
     private int estado_cao = PARADO;
 
+    private static int TAPETE_SALA = 1, VASO_SALA = 2;
+    private int ref_obj_interacao = 0;
+
     private float destino_x, destino_y, distancia_x, distancia_y;;
     private boolean chegou_destino_x, chegou_destino_y;
     private float velocidade_cao_fixa = 7.5f, velocidade_cao_x, velocidade_cao_y;
+    private boolean cao_is_safe = false;
+    private int tam_x_dog, tam_y_dog;
+
+    private int points;
+
     private AGVector2D origem = null, destino = null, hipo = null;
 
     private AGSprite sala = null;
@@ -29,6 +37,10 @@ public class CenaGame extends AGScene {
 
     private AGSprite hitbox_sofa_p, hitbox_sofa_g, hitbox_mesa_sala, hitbox_tapete_sala, hitbox_sala_safe1, hitbox_coluna_sala;
 
+    private AGSprite botao_aceitar = null;
+    private AGSprite botao_cancelar = null;
+
+
     public CenaGame(AGGameManager pManager) {
         super(pManager);
     }
@@ -36,7 +48,11 @@ public class CenaGame extends AGScene {
     @Override
     public void init() {
 
-        cria_cao();
+        tam_x_dog = 5;
+        tam_y_dog = 10;
+
+        //gerencia os locais que o cao está e suas interações
+        cria_locais(SALA);
 
         origem = new AGVector2D();
         destino = new AGVector2D();
@@ -44,11 +60,11 @@ public class CenaGame extends AGScene {
 
     }
 
-    private void cria_cao() {
+    private void cria_cao(float pos_x, float pos_y) {
 
         cao = createSprite(R.drawable.catio_sheet, 4, 1);
-        cao.setScreenPercent(5, 10);
-        cao.vrPosition.setXY(AGScreenManager.iScreenWidth/2, AGScreenManager.iScreenHeight/2);
+        cao.setScreenPercent( tam_x_dog, tam_y_dog);
+        cao.vrPosition.setXY(pos_x, pos_y);
         cao.addAnimation(4, true, 0, 3);
 
     }
@@ -66,28 +82,74 @@ public class CenaGame extends AGScene {
     @Override
     public void loop() {
 
-        //gerencia os locais que o cao está e suas interações
-        locais();
+        if(cao == null){
+            cria_cao(AGScreenManager.iScreenWidth/2, AGScreenManager.iScreenHeight/4.2f);
+        }
+
+        eventos_toque();
+
+        movimenta_cao();
+
+        verifica_botoes_interacao();
+
+
+
+
+    }
+
+    private void verifica_botoes_interacao() {
+
+        if(botao_aceitar != null){
+            if(botao_aceitar.collide(AGInputManager.vrTouchEvents.getLastPosition())){
+
+                if(ref_obj_interacao == TAPETE_SALA){
+
+                } else if(ref_obj_interacao == VASO_SALA){
+
+                }
+
+                mostrar_botoes_interacao(0, false);
+                estado_cao = PARADO;
+                return;
+            }
+
+            if(botao_cancelar.collide(AGInputManager.vrTouchEvents.getLastPosition())){
+                mostrar_botoes_interacao(0, false);
+                estado_cao = PARADO;
+                return;
+            }
+        }
+
+    }
+
+    private void eventos_toque() {
 
         //Se a tela for clicada!
         if(AGInputManager.vrTouchEvents.screenClicked()){
 
-            origem.setXY(cao.vrPosition.getX(), cao.vrPosition.getY());
-            destino.setXY(AGInputManager.vrTouchEvents.getLastPosition().fX, AGInputManager.vrTouchEvents.getLastPosition().fY);
 
-            hipo.setXY(destino.getX() - origem.getX(), destino.getY() - origem.getY());
-            double an = Math.toDegrees(  Math.atan2(hipo.getY(), hipo.getX()) - Math.PI/2);
-
-            cao.fAngle = (float) an;
 
             if(local_cao == SALA) {
-                if(AGInputManager.vrTouchEvents.getLastPosition().fX > AGScreenManager.iScreenWidth / 5.25f){
-                    if(AGInputManager.vrTouchEvents.getLastPosition().fX < AGScreenManager.iScreenWidth / 1.1f){
-                        destino_x = AGInputManager.vrTouchEvents.getLastPosition().fX;
-                    }
+
+                if((AGInputManager.vrTouchEvents.getLastPosition().fX > AGScreenManager.iScreenWidth / 7.5f) && (AGInputManager.vrTouchEvents.getLastPosition().fY < AGScreenManager.iScreenHeight / 1.2f) &&
+                    (AGInputManager.vrTouchEvents.getLastPosition().fX < AGScreenManager.iScreenWidth / 1.2f) && (AGInputManager.vrTouchEvents.getLastPosition().fY > AGScreenManager.iScreenHeight / 5.7f)){
+
+                    destino_x = AGInputManager.vrTouchEvents.getLastPosition().fX;
+                    destino_y = AGInputManager.vrTouchEvents.getLastPosition().fY;
+
+                    origem.setXY(cao.vrPosition.getX(), cao.vrPosition.getY());
+                    destino.setXY(AGInputManager.vrTouchEvents.getLastPosition().fX, AGInputManager.vrTouchEvents.getLastPosition().fY);
+
+                    hipo.setXY(destino.getX() - origem.getX(), destino.getY() - origem.getY());
+                    double an = Math.toDegrees(  Math.atan2(hipo.getY(), hipo.getX()) - Math.PI/2);
+
+                    cao.fAngle = (float) an;
+
+                    mostrar_botoes_interacao(0, false);
+
                 }
-                //destino_x = AGInputManager.vrTouchEvents.getLastPosition().fX;
-                destino_y = AGInputManager.vrTouchEvents.getLastPosition().fY;
+
+
             }
             estado_cao = ANDANDO;
 
@@ -132,10 +194,20 @@ public class CenaGame extends AGScene {
 
         }
 
+    }
+
+    private void movimenta_cao() {
+
+
+
         //verifica se o estado do cão é o de andando ou parado
         if(estado_cao == ANDANDO){
 
-            Log.i("ASDF", "velx: "+velocidade_cao_x + " |vely: "+velocidade_cao_y);
+            if(cao != null) {
+
+                checa_collide_hitbox(cao);
+
+            }
 
             if(!chegou_destino_x) {
                 if (cao.vrPosition.getX() > destino_x) {
@@ -192,11 +264,82 @@ public class CenaGame extends AGScene {
         }
 
 
+
+
     }
 
-    private void locais() {
+    private void checa_collide_hitbox(AGSprite sprite) {
 
-        if (local_cao == SALA){
+        if (local_cao == SALA) {
+
+            if(hitbox_tapete_sala.collide(sprite)){
+
+                mostrar_botoes_interacao(0, true);
+
+            } else {
+
+            }
+
+            if (hitbox_sofa_p.collide(sprite) || hitbox_sofa_g.collide(sprite) || hitbox_mesa_sala.collide(sprite) || hitbox_coluna_sala.collide(sprite)) {
+
+                if (sprite.vrPosition.getX() > destino_x) {
+                    sprite.vrPosition.setX(sprite.vrPosition.getX() + velocidade_cao_x * 2);
+                } else {
+                    sprite.vrPosition.setX(sprite.vrPosition.getX() - velocidade_cao_x * 2);
+                }
+
+                if (sprite.vrPosition.getY() > destino_y) {
+                    sprite.vrPosition.setY(sprite.vrPosition.getY() + velocidade_cao_y * 2);
+                } else {
+                    sprite.vrPosition.setY(sprite.vrPosition.getY() - velocidade_cao_y * 2);
+                }
+
+                estado_cao = PARADO;
+            }
+
+
+
+            if (hitbox_sala_safe1.collide(sprite)) {
+                cao_is_safe = true;
+            } else {
+                cao_is_safe = false;
+            }
+        }
+
+    }
+
+    private void mostrar_botoes_interacao(int interacao, boolean mostrar){
+
+        if(mostrar){
+            if(botao_aceitar == null){
+                botao_aceitar = createSprite(R.drawable.checked, 1, 1);
+                botao_aceitar.vrPosition.setXY(AGScreenManager.iScreenWidth/1.5f, AGScreenManager.iScreenHeight/4);
+                botao_aceitar.setScreenProportional(AGScreenManager.iScreenWidth/10);
+
+                botao_cancelar = createSprite(R.drawable.cancel, 1, 1);
+                botao_cancelar.vrPosition.setXY(AGScreenManager.iScreenWidth/4f, AGScreenManager.iScreenHeight/4);
+                botao_cancelar.setScreenProportional(AGScreenManager.iScreenWidth/10);
+
+                ref_obj_interacao = interacao;
+
+            }
+        } else {
+            if(botao_aceitar != null){
+                removeSprite(botao_aceitar);
+                botao_aceitar = null;
+                removeSprite(botao_cancelar);
+                botao_cancelar = null;
+                ref_obj_interacao = 0;
+            }
+        }
+
+    }
+
+    private void cria_locais(int local) {
+
+        if (local == SALA){
+
+            remove_todos_oslocais();
 
             sala = createSprite(R.drawable.sala, 1,1);
             sala.setScreenPercent(100, 100);
@@ -232,11 +375,31 @@ public class CenaGame extends AGScene {
             hitbox_coluna_sala.vrPosition.setXY(AGScreenManager.iScreenWidth/6.5f, AGScreenManager.iScreenHeight/5.1f);
             hitbox_coluna_sala.setColor(1,1,1,0);
 
-            if(hitbox_sofa_p.collide(cao)){
-                estado_cao = PARADO;
-            }
 
-            cria_cao();
+        }
+
+    }
+
+    private void remove_todos_oslocais() {
+
+        //remove sala se tiver instanciado
+        if(sala != null) {
+
+            removeSprite(sala);
+            removeSprite(hitbox_sala_safe1);
+            removeSprite(hitbox_sofa_p);
+            removeSprite(hitbox_sofa_g);
+            removeSprite(hitbox_mesa_sala);
+            removeSprite(hitbox_tapete_sala);
+            removeSprite(hitbox_coluna_sala);
+
+            sala = null;
+            hitbox_sala_safe1 = null;
+            hitbox_sofa_p = null;
+            hitbox_sofa_g = null;
+            hitbox_mesa_sala = null;
+            hitbox_tapete_sala = null;
+            hitbox_coluna_sala = null;
 
         }
 
